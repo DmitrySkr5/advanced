48c5f600a8dd824e6d1637b56575608cf4dd27b1
DmitrySkr5 <dimaskr55@gmail.com>
2024-08-19 19:15:00 +0300

    first commit

---
+++ manageu/classses/TaskManager.js
@@ -0,0 +1,28 @@
+
+import Task from "./Task.js";
+
+class TaskManager {
+    constructor() {
+        this.tasks = [];
+    }
+
+    addTask(description) {
+        this.tasks.push(new Task(description));
+    }
+
+    deleteTask(id) {
+        this.task = this.tasks.filter((task) => task.id != id);
+    }
+
+    updateTaskDescription(id, newDesc) {
+        let indexToUpdate = this.tasks.findIndex((task) => task.id == id);
+        this.tasks[indexToUpdate].description = newDesc;
+    }
+
+    completeTask() {
+        let indexToUpdate = this.tasks.findIndex((task) => task.id == id);
+        this.tasks[indexToUpdate].completed = true;
+    }
+}
+
+export default TaskManager;
---
+++ manageu/script.js
@@ -0,0 +1,10 @@
+
+
+
+
+let manager = new TaskManager();
+console.log(manager);
+
+manager.addTask("HomeWork");
+manager.addTask("Clean the mmd");
+console.log(manager);
